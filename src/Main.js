2549d0afffed323f4f3b487a3314c2999f8401a1
Noah Salaman <nsalaman@iu.edu>
2018-06-12 14:26:22 -0400

    added chat header

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,18 @@ import Sidebar from './Sidebar'
 class Main extends Component {
   render() {
     return (
-      <div className = "Main">
-        <Chat />
+      <div className = "Main" style={styles}>
         <Sidebar />
+        <Chat />
       </div>
     )
   }
+}
+
+const styles = {
+  display: 'flex',
+  alignItems: 'strech',
+  height: '100vh',
 }
 
 export default Main;

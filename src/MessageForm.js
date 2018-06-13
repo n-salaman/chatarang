53eacd05b4c1cba68f7eb89e2e7cdcf1fac53706
Noah Salaman <nsalaman@iu.edu>
2018-06-13 13:41:54 -0400

    autoFoucs input

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -8,6 +8,7 @@ class MessageForm extends Component {
   handleSubmit = (ev) => {
     ev.preventDefault()
     this.props.addMessage(this.state.body)
+    this.setState({body: ' '})
   }
 
   handleChange = (ev) => {
@@ -28,6 +29,7 @@ class MessageForm extends Component {
           placeholder="Type a message..."
           value={this.state.body}
           onChange={this.handleChange}
+          autoFocus
         />
         <button type="submit" style={styles.MessageFormButton}>Send</button>
       </form>
